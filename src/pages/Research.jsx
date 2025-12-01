b97b5deaca6254a097247d8f029b49f7fe256757
chemilk02 <chemilk02@gmail.com>
2025-12-01 18:51:43 +0900

    color update

--- src/pages/Research.jsx
+++ src/pages/Research.jsx
@@ -1,47 +1,19 @@
 import { useParams } from 'react-router-dom'
 import PageTitle from '../components/common/PageTitle'
+import researchData from '../data/research.json'
 
 function Research() {
   const { tab } = useParams()
-  const currentTab = tab || 'research1'
+  const currentTab = tab || 'aerospace'
 
   const tabs = [
-    { id: 'research1', label: 'Research 1', path: '/research' },
-    { id: 'research2', label: 'Research 2', path: '/research/research2' },
-    { id: 'research3', label: 'Research 3', path: '/research/research3' },
-    { id: 'research4', label: 'Research 4', path: '/research/research4' },
-    { id: 'research5', label: 'Research 5', path: '/research/research5' }
+    { id: 'aerospace', label: 'Aerospace', path: '/research' },
+    { id: 'defense', label: 'Defense', path: '/research/defense' },
+    { id: 'ai-analysis', label: 'AI Analysis', path: '/research/ai-analysis' },
+    { id: 'super-alloy', label: 'Super Alloy', path: '/research/super-alloy' }
   ]
 
-  const researchContent = {
-    research1: {
-      title: 'Aerospace',
-      subtitle: 'sub title of the research',
-      content: 'explain about research Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
-    },
-    research2: {
-      title: 'Super Alloy',
-      subtitle: 'sub title of the research',
-      content: 'explain about research Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
-    },
-    research3: {
-      title: 'Research 3',
-      subtitle: 'sub title of the research',
-      content: 'explain about research Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
-    },
-    research4: {
-      title: 'Research 4',
-      subtitle: 'sub title of the research',
-      content: 'explain about research Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
-    },
-    research5: {
-      title: 'Research 5',
-      subtitle: 'sub title of the research',
-      content: 'explain about research Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
-    }
-  }
-
-  const content = researchContent[currentTab]
+  const content = researchData[currentTab]
 
   return (
     <>
@@ -52,14 +24,21 @@ function Research() {
         currentTab={currentTab}
       />
       <div className="banner-img">
-        <img src="" style={{ height: '300px' }} alt="" />
+        <img 
+          src={content?.image || ''} 
+          style={{ height: '300px', width: '100%', objectFit: 'cover' }} 
+          alt={content?.title || 'Research Image'} 
+        />
       </div>
-      <h1>{content.title}</h1>
-      <h3>{content.subtitle}</h3>
-      <h6>{content.content}</h6>
+      <h1>{content?.title}</h1>
+      <h3>{content?.subtitle}</h3>
+      <h6 style={{ whiteSpace: 'pre-line' }}>
+        {content?.contentEn}
+        <br /><br />
+        {content?.contentKo}
+      </h6>
     </>
   )
 }
 
 export default Research
-
